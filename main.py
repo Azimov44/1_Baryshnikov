f3372360b93a49e1d5f08190995188c57f5f80f4
Azimov44 <viktor.299@mail.ru>
2023-05-22 14:38:22 +0300

    файл с кодом

---
+++ main.py
@@ -0,0 +1,13 @@
+n = int(input("Введите количество чисел: "))
+max_even = None
+
+for i in range(n):
+    num = int(input(f"Введите число {i+1}: "))
+    
+    if num % 2 == 0 and (max_even is None or num > max_even):
+        max_even = num
+
+if max_even:
+    print(f"Наибольшее четное число: {max_even}")
+else:
+    print("В последовательности нет четных чисел")
